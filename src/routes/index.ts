dcaef77a834f514cf0712319d136426c98497474
Alfiado Constantino Ngana <alfiado02@gmail.com>
2020-06-26 19:32:51 +0200

    Create User

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,12 @@
 import { Router } from 'express';
 
 import appointmentsRoutes from './appointments.routes';
+import userRoute from './users.routes';
 
 const routes = Router();
 
 routes.use('/appointments', appointmentsRoutes);
+
+routes.use('/users', userRoute);
 
 export default routes;
