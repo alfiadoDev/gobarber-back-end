a51d3972ee4da5ceb433b90ef157e79f30acb992
Alfiado Constantino Ngana <alfiado02@gmail.com>
2020-06-29 20:51:15 +0200

    Authentication

---
+++ src/config/auth.ts
@@ -0,0 +1,6 @@
+export default {
+  jwt: {
+    secret: '00db7a7cbe8ab646192559b0efb737c4',
+    expireIn: '1d',
+  },
+};
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,11 +2,14 @@ import { Router } from 'express';
 
 import appointmentsRoutes from './appointments.routes';
 import userRoute from './users.routes';
+import sessionRouter from './sessions.routes';
 
 const routes = Router();
 
 routes.use('/appointments', appointmentsRoutes);
 
 routes.use('/users', userRoute);
+
+routes.use('/sessions', sessionRouter);
 
 export default routes;
