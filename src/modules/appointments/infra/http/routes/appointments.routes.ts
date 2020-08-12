84d62025b40c22d53d78845c3e968ded8f7f31d9
Alfiado Constantino Ngana <alfiado02@gmail.com>
2020-08-12 20:58:20 +0200

    Dependency Inversion Principle letra D do SOLID

--- src/modules/appointments/infra/http/routes/appointments.routes.ts
+++ src/modules/appointments/infra/http/routes/appointments.routes.ts
@@ -1,30 +1,30 @@
 import { Router } from 'express';
 import { parseISO } from 'date-fns';
-import { getCustomRepository } from 'typeorm';
 
-import AppointmentsRepository from '@modules/appointments/repositories/AppointmentsRepository';
+import AppointmentsRepository from '@modules/appointments/infra/typeorm/repositories/AppointmentsRepository';
 import CreateAppointmentService from '@modules/appointments/services/CreateAppointmentService';
 
 import ensureAuthenticated from '@modules/users/infra/http/middlewares/ensureAuthenticated';
 
 const appointmentsRoutes = Router();
+const appointmentsRepository = new AppointmentsRepository();
 
 appointmentsRoutes.use(ensureAuthenticated);
 
-appointmentsRoutes.get('/', async (request, response) => {
-  const appointmentsRepository = getCustomRepository(AppointmentsRepository);
+// appointmentsRoutes.get('/', async (request, response) => {
+//   const appointments = await appointmentsRepository.find();
 
-  const appointments = await appointmentsRepository.find();
-
-  return response.json(appointments);
-});
+//   return response.json(appointments);
+// });
 
 appointmentsRoutes.post('/', async (request, response) => {
   const { provider_id, date } = request.body;
 
   const parseDate = parseISO(date);
 
-  const createAppointmentService = new CreateAppointmentService();
+  const createAppointmentService = new CreateAppointmentService(
+    appointmentsRepository,
+  );
 
   const appointment = await createAppointmentService.execute({
     date: parseDate,
