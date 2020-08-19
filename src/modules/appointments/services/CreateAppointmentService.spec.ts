7ca5c4078ef52091ac7bac71cb15b41b2f03f82b
Alfiado Constantino Ngana <alfiado02@gmail.com>
2020-08-20 00:20:32 +0200

    pensando nos testes e criando o primeiro teste unitario

--- src/modules/appointments/services/CreateAppointmentService.spec.ts
+++ src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -1,3 +1,22 @@
-test('sum two numbers', () => {
-  expect(1 + 2).toBe(3);
+import FakeAppointmentRepoitory from '../repositories/fakes/FakeAppointmentRepository';
+import CreateAppointmentService from './CreateAppointmentService';
+
+describe('CreateAppointment', () => {
+  it('should be able to create a new appointment', async () => {
+    const fakeAppointmentRepoitory = new FakeAppointmentRepoitory();
+    const createAppointmentService = new CreateAppointmentService(
+      fakeAppointmentRepoitory,
+    );
+
+    const appointment = await createAppointmentService.execute({
+      date: new Date(),
+      provider_id: '1234556474',
+    });
+
+    expect(appointment).toHaveProperty('id');
+    expect(appointment.provider_id).toBe('1234556474');
+  });
+  // it('should not be able to create two appointments on the same time', () => {
+  //   expect(1 + 2).toBe(3);
+  // });
 });
