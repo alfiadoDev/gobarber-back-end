4895702bc7692f9d42d33ccbb913269ca003177e
Alfiado Constantino Ngana <alfiado02@gmail.com>
2020-08-20 13:57:15 +0200

    testes de autenticacao

--- src/modules/users/services/AuthenticateUserService.spec.ts
+++ src/modules/users/services/AuthenticateUserService.spec.ts
@@ -1,3 +1,4 @@
+import AppError from '@shared/errors/AppError';
 import FakeUserRepository from '../repositories/fakes/FakeUserRepository';
 import FakeHashProvider from '../providers/HashProvider/fakes/FakeHashProvider';
 import AuthenticateUserService from './AuthenticateUserService';
@@ -28,5 +29,48 @@ describe('AuthenticateUser', () => {
     });
 
     expect(response).toHaveProperty('token');
+  });
+
+  it('should not be able to auhtenticate user with no exists user', async () => {
+    const fakeUserRepoitory = new FakeUserRepository();
+    const fakeHashProvider = new FakeHashProvider();
+
+    const authenticateUserService = new AuthenticateUserService(
+      fakeUserRepoitory,
+      fakeHashProvider,
+    );
+
+    expect(
+      authenticateUserService.execute({
+        email: 'email@email.com',
+        password: '123456',
+      }),
+    ).rejects.toBeInstanceOf(AppError);
+  });
+
+  it('should not be able to auhtenticate user with wrong password', async () => {
+    const fakeUserRepoitory = new FakeUserRepository();
+    const fakeHashProvider = new FakeHashProvider();
+    const createUserService = new CreateUserService(
+      fakeUserRepoitory,
+      fakeHashProvider,
+    );
+    const authenticateUserService = new AuthenticateUserService(
+      fakeUserRepoitory,
+      fakeHashProvider,
+    );
+
+    await createUserService.execute({
+      name: 'meu nome',
+      email: 'email@email.com',
+      password: '123456',
+    });
+
+    expect(
+      authenticateUserService.execute({
+        email: 'email@email.com',
+        password: '123456nbnbnb',
+      }),
+    ).rejects.toBeInstanceOf(AppError);
   });
 });
