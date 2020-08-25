0c1a5e6ac3c9e22a52e6570bb40e589f014e31bb
Alfiado Constantino Ngana <alfiado02@gmail.com>
2020-08-25 21:16:12 +0200

    template de emails

--- src/shared/container/providers/index.ts
+++ src/shared/container/providers/index.ts
@@ -6,11 +6,18 @@ import DisckStorageProvider from './storageProvider/implementations/DisckStorage
 import IMailerProvider from './MailProvider/models/IMailProvider';
 import EtherealMailProvider from './MailProvider/implementations/EtherealMailProvider';
 
+import IMailTemplateProvider from './MailTemplateProvider/models/IMailTemplateProvider';
+import HandlebarsMailTemplateProvider from './MailTemplateProvider/implementations/HandlebarsMailTemplateProvider';
+
 container.registerSingleton<IStorageProvider>(
   'StorageProvider',
   DisckStorageProvider,
 );
+container.registerSingleton<IMailTemplateProvider>(
+  'MailTemplateProvider',
+  HandlebarsMailTemplateProvider,
+);
 container.registerInstance<IMailerProvider>(
   'MailProvider',
-  new EtherealMailProvider(),
+  container.resolve(EtherealMailProvider),
 );
