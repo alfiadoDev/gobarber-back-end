11896675728fa1f0836cdac1760c718676772bae
Alfiado Constantino Ngana <alfiado02@gmail.com>
2020-08-25 19:35:26 +0200

    salvando os tokens no banco

--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -3,6 +3,7 @@ import { Router } from 'express';
 import appointmentsRoutes from '@modules/appointments/infra/http/routes/appointments.routes';
 import userRoute from '@modules/users/infra/http/routes/users.routes';
 import sessionRouter from '@modules/users/infra/http/routes/sessions.routes';
+import passwordRouter from '@modules/users/infra/http/routes/password.routes';
 
 const routes = Router();
 
@@ -11,5 +12,7 @@ routes.use('/appointments', appointmentsRoutes);
 routes.use('/users', userRoute);
 
 routes.use('/sessions', sessionRouter);
+
+routes.use('/password', passwordRouter);
 
 export default routes;
