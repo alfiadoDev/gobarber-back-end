99c1d28fc436719a43f231e8440dfdfe03241a2a
Alfiado Constantino <alfiado02@gmail.com>
2020-10-24 14:41:58 +0200

    upload de arquivos para o amazon s3

--- src/shared/container/providers/storageProvider/index.ts
+++ src/shared/container/providers/storageProvider/index.ts
@@ -1,13 +1,17 @@
 import { container } from 'tsyringe';
 
+import uploadConfig from '@config/upload';
+
 import IStorageProvider from './models/IStorageProvider';
 import DisckStorageProvider from './implementations/DisckStorageProvider';
+import S3StorageProvider from './implementations/S3StorageProvider';
 
 const providers = {
   disck: DisckStorageProvider,
+  s3: S3StorageProvider,
 };
 
 container.registerSingleton<IStorageProvider>(
   'StorageProvider',
-  providers.disck,
+  providers[uploadConfig.driver],
 );
